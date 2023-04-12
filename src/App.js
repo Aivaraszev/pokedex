6305a1a174d93d6700704b45a0bbbce6ef020d81
Aivaraszev <36788893+Aivaraszev@users.noreply.github.com>
2023-04-12 19:15:30 +0200

    Set up routing and page state.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import { Outlet } from "react-router-dom";
+import { useState } from "react";
 import "./App.css";
 
 import { ChakraProvider } from "@chakra-ui/react";
 
 function App() {
+    const [page, setPage] = useState(0);
     return (
         <ChakraProvider>
             <div className="App">
                 <header className="App-header">
-                    <Outlet />
+                    <Outlet context={[page, setPage]} />
                 </header>
             </div>
         </ChakraProvider>
--- src/index.js
+++ src/index.js
@@ -3,11 +3,22 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { RouterProvider, createHashRouter } from "react-router-dom";
-
+import PokemonCard from "./PokemonCard";
+import PokemonList from "./PokemonList";
 const router = createHashRouter([
     {
         path: "/",
         element: <App />,
+        children: [
+            {
+                path: "/",
+                element: <PokemonList />,
+            },
+            {
+                path: "/pokemon/:name",
+                element: <PokemonCard />,
+            },
+        ],
     },
 ]);
 const root = ReactDOM.createRoot(document.getElementById("root"));
